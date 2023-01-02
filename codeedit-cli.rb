f249ce0276edefcb5a873941c33a97f7cab7a3ce
Lukas Pistrol <lukas@pistrol.com>
2023-01-02 16:20:58 +0100

    add codeedit-cli formula

---
+++ codeedit-cli.rb
@@ -0,0 +1,18 @@
+class CodeeditCli < Formula
+  desc "A command-line-interface to open files/folders in CodeEdit.app"
+  homepage "https://github.com/CodeEditApp/CodeEditCLI"
+  url "https://github.com/CodeEditApp/CodeEditCLI.git", tag: "0.0.6"
+  head "https://github.com/CodeEditApp/CodeEditCLI.git", branch: "main"
+  license "MIT"
+  version "0.0.6"
+
+  depends_on xcode: ["14.0", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "true"
+  end
+end
