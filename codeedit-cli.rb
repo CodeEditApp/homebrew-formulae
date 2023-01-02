b6482e344f6f4bd31c3a4f75f16fd4fe0ca24ae0
Lukas Pistrol <lukas@pistrol.com>
2023-01-02 16:45:30 +0100

    add dependencies

--- codeedit-cli.rb
+++ codeedit-cli.rb
@@ -1,12 +1,13 @@
 class CodeeditCli < Formula
   desc "A command-line-interface to open files/folders in CodeEdit.app"
   homepage "https://github.com/CodeEditApp/CodeEditCLI"
-  url "https://github.com/CodeEditApp/CodeEditCLI.git", tag: "0.0.7"
-  head "https://github.com/CodeEditApp/CodeEditCLI.git", branch: "main"
+  url "https://github.com/CodeEditApp/CodeEditCLI/archive/refs/tags/0.0.7.tar.gz"
+  sha256 "6556e0d74938d68a53862600dce1d9f270570c16825e615d673f584193ffe5aa"
   license "MIT"
-  version "0.0.7"
 
   depends_on xcode: ["14.0", :build]
+  depends_on :macos
+  uses_from_macos "swift"
 
   def install
     system "make", "install", "prefix=#{prefix}"
