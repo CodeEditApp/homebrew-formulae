f03b7208c214cfe05bfd5ae642f028401e8f09ed
Lukas Pistrol <lukas@pistrol.com>
2023-01-12 04:48:15 +0100

    update to version 0.0.9

--- codeedit-cli.rb
+++ codeedit-cli.rb
@@ -1,8 +1,8 @@
 class CodeeditCli < Formula
   desc "A command-line-interface to open files/folders in CodeEdit.app"
   homepage "https://github.com/CodeEditApp/CodeEditCLI"
-  url "https://github.com/CodeEditApp/CodeEditCLI/archive/refs/tags/0.0.8.tar.gz"
-  sha256 "b459d7a22b418d8f895ebb7158781f225855c77e1dbf4b5a38dad951f3aa5221"
+  url "https://github.com/CodeEditApp/CodeEditCLI/archive/refs/tags/0.0.9.tar.gz"
+  sha256 "ffe71850771f8c04a65f6b1252b179caf129ccdd6abacb15b1882c17e2f237a0"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
